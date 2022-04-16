cbe4d052468d514f78add7ee4c3bf1f8a0c60d9b
WojtekMs <wojtek.atmo@gmail.com>
2022-04-16 14:06:55 +0200

    Added another implementation of to_binary

--- algorithms/binary_conversions/to_binary.cpp
+++ algorithms/binary_conversions/to_binary.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <string>
+#include <algorithm>
 #include <ranges>
+
 
 std::string to_binary(const int decimal) {
     constexpr int bits = sizeof(decimal) * 8;
@@ -25,12 +27,24 @@ std::string to_binary(const int decimal) {
 
 }
 
+std::string to_binary2(int decimal) {
+    std::string result{};
+    while (decimal) {
+        result.push_back((decimal & 1) + '0');
+        decimal /= 2;
+    }
+    std::ranges::reverse(result);
+    return result;
+}
+
 int main(int argc, const char** argv) {
     if (argc != 2) {
         std::cout << "usage: " << argv[0] << " decimal_number\n";
         return EXIT_FAILURE;
     }
     const int decimal = atoi(argv[1]);
+    std::cout << to_binary(decimal) << "\n";
     std::cout << r::to_binary(decimal) << "\n";
+    std::cout << to_binary2(decimal) << "\n";
     return EXIT_SUCCESS;
 }
